bbc61baaf35fec55195b1cbe267d067034c0183e
Steve Goodhill <s.goodhill@protonmail.com>
2021-10-06 13:23:26 +1000

    [PM-04] Updated pet detail screen

--- lib/screens/pet/pet_detail_screen.dart
+++ lib/screens/pet/pet_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:petmo/models/pet/pet.dart';
+import 'package:petmo/widgets/bottom_nav_widget.dart';
 
 import '../style.dart';
 
@@ -15,11 +16,14 @@ class PetDetailScreen extends StatefulWidget {
 }
 
 class _PetDetailScreenState extends State<PetDetailScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late String name;
   bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
+    name = widget.pet.name;
   }
 
   @override
@@ -29,28 +33,67 @@ class _PetDetailScreenState extends State<PetDetailScreen> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Petmo',
-          textAlign: TextAlign.center,
-          style: AppBarTextStyle,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Petmo',
+            textAlign: TextAlign.center,
+            style: AppBarTextStyle,
+          ),
         ),
-      ),
-      body: ListView(
-              children: [
-                buildName(widget.pet),
-                buildCreated(widget.pet),
-                buildRating(widget.pet),
-              ],
-            ));
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: ListView(children: [
+                  buildName(widget.pet),
+                  buildCreated(widget.pet),
+                  buildAge(widget.pet),
+                  buildRating(widget.pet),
+                ]),
+              ),
+              Expanded(
+                child: BottomNavWidget(pet: widget.pet),
+              ),
+              // buildBottomNav(),
+            ]),
+      );
 
   Widget buildName(Pet pet) => Padding(
         padding: const EdgeInsets.all(12),
-        child: Text(
-          pet.name,
-          style: TitleTextStyle,
-          textAlign: TextAlign.center,
+        child: TextFormField(
+          maxLines: 1,
+          initialValue: name,
+          style: const TextStyle(
+            color: SecondaryAccentColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+          cursorColor: SecondaryAccentColor,
+          decoration: const InputDecoration(
+
+            // enabledBorder: OutlineInputBorder(
+            //   borderSide: BorderSide(color: SecondaryAccentColor),
+            //   borderRadius: BorderRadius.all(Radius.circular(15)),
+            // ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: DarkAccentColor),
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            // labelText: 'Enter A Name',
+            // labelStyle: TextStyle(
+            //   color: SecondaryAccentColor,
+            // ),
+            floatingLabelStyle: TextStyle(
+              color: SecondaryAccentColor,
+            ),
+            suffixIcon: Icon(Icons.edit, color: DarkAccentColor),
+            suffixStyle: TitleTextStyle,
+          ),
+          validator: (name) =>
+              name != null && name.isEmpty ? 'The name cannot be empty' : null,
+          // onChanged: onChangedName,
         ),
       );
 
@@ -58,6 +101,16 @@ class _PetDetailScreenState extends State<PetDetailScreen> {
         padding: const EdgeInsets.all(12),
         child: Text(
           'Created: ' + DateFormat.yMMMd().format(pet.created),
+          style: Body1TextStyle,
+          textAlign: TextAlign.center,
+        ),
+      );
+
+  Widget buildAge(Pet pet) => Padding(
+        padding: const EdgeInsets.all(12),
+        child: Text(
+          'Age (Days): ' +
+              DateTime.now().difference(pet.created).inDays.toString(),
           style: Body1TextStyle,
           textAlign: TextAlign.center,
         ),
