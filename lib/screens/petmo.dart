798d4116ac9458dbb474e804beff41294aa803b8
Steve Goodhill <s.goodhill@protonmail.com>
2021-10-07 15:07:59 +1000

    [PM-06] Added Firebase plugins and initial integration, doesn't do anything yet

--- lib/screens/petmo.dart
+++ lib/screens/petmo.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:petmo/screens/pet/pet_detail_screen.dart';
 import 'package:petmo/screens/pet/pet_screen.dart';
@@ -10,9 +12,12 @@ const PetDetailScreenRoute = '/pet_detail';
 class Petmo extends StatelessWidget {
   const Petmo({Key? key}) : super(key: key);
 
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(onGenerateRoute: _routes(), theme: _theme());
+    return MaterialApp(onGenerateRoute: _routes(), theme: _theme(), navigatorObservers: <NavigatorObserver>[observer]);
   }
 
   RouteFactory _routes() {
